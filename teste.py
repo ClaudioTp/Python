4b31ffa7539aa1da82cc7b8ce9f7d12657b03972
Claudio soares da silva <117151867+ClaudioTp@users.noreply.github.com>
2022-12-26 21:05:43 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,9 +0,0 @@
-A = float(input("Digite o Valor da Base A : "))
-B = float(input("Digite o Valor da Base B: "))
-C = float(input("Digite o Valor da altura: "))
- 
-if A >= 0 and B >= 0 and C >= 0 : 
-    area =(A + B) * C /2
-    print('A area É %.1f' %(area)) 
-else:
-    print("Nao Foi Possivel Calcular")
